9381ad3bda07fb5df510695dec16a2fdf9d9f2f5
Manu <>
2022-08-10 00:16:50 -0500

    first commit

---
+++ example.js
@@ -0,0 +1,4 @@
+const { Calculator } = require("manutoalap-calculator")
+let _calculator = new Calculator();
+console.log(_calculator.sum(1,2));
+console.log(_calculator.pow('1a',2));
---
+++ index.js
@@ -0,0 +1,10 @@
+class Calculator{
+    sum(a,b){
+        return a+b;
+    }
+    pow(base,exponent){
+        return Math.pow(base,exponent);
+    }
+}
+
+module.exports = {Calculator};
